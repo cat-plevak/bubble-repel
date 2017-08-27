0de92acefffbdc5c051f591ba18167fcb8242cf8
cat-plevak <catherine.plevak@colorado.edu>
2017-08-27 15:41:32 -0600

    playing with forcelayout

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// d3.select('.item').text('select');
+// d3.select('.item:nth-child(2)').text('select');
+// NOTE: D3 BEGINS COUNT AT 1, NOT 0
+
+
+// selects every third div with class of 'item' and changes html to 'selection' with strong tag
+// d3.selectAll('.item:nth-child(3n)')
+//   .html('<strong>selection</strong>');
+
+
+// puts item at end of list
+// d3.select('.item')
+//   .append('span')
+//   .html(' <strong>selection</strong>');
+
+
+// removes element
+  // .remove()
+  
